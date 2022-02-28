a0a704a5a02a301e689c6ad1420aeed4bff9a215
Mk13Ghoul <mayankkumarj5@gmail.com>
2022-02-28 17:17:22 +0530

    Updates

---
+++ precache-manifest.60ba8665b066772a4f5ece8e51ddcadb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24803f42d02f1db322e8a6b72e12100e",
+    "url": "/25-5-Clock/index.html"
+  },
+  {
+    "revision": "27bf8389c1613e2ccf3e",
+    "url": "/25-5-Clock/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "d69e8b5333e087bfaac6",
+    "url": "/25-5-Clock/static/css/main.2d4e9e6f.chunk.css"
+  },
+  {
+    "revision": "27bf8389c1613e2ccf3e",
+    "url": "/25-5-Clock/static/js/2.92cdcad9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/25-5-Clock/static/js/2.92cdcad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69e8b5333e087bfaac6",
+    "url": "/25-5-Clock/static/js/main.0d304ac2.chunk.js"
+  },
+  {
+    "revision": "f1e9068fb91c966f022d",
+    "url": "/25-5-Clock/static/js/runtime-main.3d8eb369.js"
+  }
+]);
